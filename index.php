f173adb73b2792cf4c21297e35e8687d2597a7e8
Ritendra <ritendra.gour@clickspurr.com>
2023-08-24 22:06:30 +0530

    working on style

--- index.php
+++ index.php
@@ -7,44 +7,48 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
+	<!-- require('navbar.php') -->
 	<div class="main-container">
-<form action="welcome.php" method="post">
-	<h1>HEllo</h1>
-	<label for="">First Name</label>
-		<input type="text" name="fname" placeholder="First Name" required>
+		<form action="welcome.php" method="post">
+			<h2>Hello This is Testing Version 2.0</h2>
 
-	<label for="">Last Name</label>
-		<input type="text" name="lname" placeholder="Last Name">
-
-	<label for="">E-mail / username</label>
-		<input type="email" name="email" placeholder="Last Name" required>
-
-	<label for="">Phone</label>
-		<input type="text" name="phone" placeholder="Last Name">
-
-	<label for="">Password</label>
-	<input type="password" name="password" placeholder="Last Name" required>
+			<label for="">Full Name</label>
+			<div class="df">
+					<input type="text" name="fname" placeholder="First Name" class="w-50" required>
 	
-	<label for="">Confirm Password</label>
-	<input type="password" name="cpassword" placeholder="Last Name" required>
+					<input type="text" name="lname" placeholder="Last Name" class="w-50">
+			</div>
 
-	<label for="">Gender</label>
-	<select name="gender" required>
-		<option value="M">Male</option>
-		<option value="F">Female</option>
-		<option value="Other">Other</option>
-	</select>
+			<label for="">E-mail</label>
+				<input type="email" name="email" placeholder="Last Name" required>
 
-	<label for="">Location</label>
-	<input type="text" name="location" placeholder="Last Name">
+			<label for="">Phone</label>
+				<input type="text" name="phone" placeholder="Last Name">
 
-	<input type="submit" class="submit">
-</form>
+			<label for="">Password</label>
+			<input type="password" name="password" placeholder="Last Name" required>
+			
+			<label for="">Confirm Password</label>
+			<input type="password" name="cpassword" placeholder="Last Name" required>
 
-<div class="image-section">
-	<!-- <img src="https://images.pexels.com/photos/17821306/pexels-photo-17821306/free-photo-of-landscape-of-hills-and-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="35%"> -->
-</div>
-</div>
+			<label for="">Gender</label>
+			<select name="gender" required>
+				<option value="M">Male</option>
+				<option value="F">Female</option>
+				<option value="Other">Other</option>
+			</select>
 
+			<label for="">Location</label>
+			<input type="text" name="location" placeholder="Last Name">
+		<div class="sub-con">
+
+			<input type="submit" class="submit">
+		</div>
+		</form>
+
+		<div class="image-section">
+			<!-- <img src="https://images.pexels.com/photos/17821306/pexels-photo-17821306/free-photo-of-landscape-of-hills-and-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="35%"> -->
+		</div>
+	</div>
 </body>
 </html>
