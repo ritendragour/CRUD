a9bc4060ae414de6517be205bcd97e0208d1f02b
Ritendra <ritendra.gour@clickspurr.com>
2023-09-01 13:05:38 +0530

    added new database

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 31, 2023 at 11:52 AM
+-- Generation Time: Sep 01, 2023 at 09:34 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -45,7 +45,26 @@ CREATE TABLE `info` (
 --
 
 INSERT INTO `info` (`id`, `fname`, `lname`, `email`, `password`, `phone`, `gender`, `location`, `role`, `dt`) VALUES
-(4, 'Ritendra', 'gour', 'ritendragour5@gmail.com', 'Ritendra@30', '', 'Male', 'Seoni Malwa', '10', '2023-08-31 15:22:14');
+(1, 'Ritendra', 'gour', 'ritendragour5@gmail.com', '$2y$10$w7kSU0H7/5VnimFBf1Ez2e43Qc5AJtBjJl4XQ5zXvvjErz59s5f5i', '+919826964395', 'Male', 'Seoni Malwa', '10', '2023-09-01 12:57:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `systemconfig`
+--
+
+CREATE TABLE `systemconfig` (
+  `id` int(11) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `dt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `systemconfig`
+--
+
+INSERT INTO `systemconfig` (`id`, `company_name`, `dt`) VALUES
+(1, 'Testing Version 2.0', '2023-09-01 12:58:06');
 
 --
 -- Indexes for dumped tables
@@ -59,6 +78,12 @@ ALTER TABLE `info`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `systemconfig`
+--
+ALTER TABLE `systemconfig`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,7 +91,13 @@ ALTER TABLE `info`
 -- AUTO_INCREMENT for table `info`
 --
 ALTER TABLE `info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `systemconfig`
+--
+ALTER TABLE `systemconfig`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
